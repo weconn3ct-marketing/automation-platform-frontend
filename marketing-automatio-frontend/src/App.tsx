441c93c4a13a6615923de8517968bda89b551384
Thagshan Arulsivam <158847977+Thagshan01@users.noreply.github.com>
2026-01-22 23:03:30 +0530

    Initial commit

---
+++ marketing-automatio-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">
+        Marketing Automation Platform 🚀
+      </h1>
+    </div>
+  );
+};
+
+export default App;
